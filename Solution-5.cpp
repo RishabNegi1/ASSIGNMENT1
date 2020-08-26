7a6a2d88efde1139f7b742ece63a0a4f9fa0ef11
RishabNegi1 <70259830+RishabNegi1@users.noreply.github.com>
2020-08-26 14:18:57 +0530

    Create Solution-5.cpp

---
+++ Solution-5.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char str[30];
+    int i,flag=0,l;
+    cout<<"enter a string"<<endl;
+    gets(str);
+    l=strlen(str);
+    for(i=0;i<l;i++)
+    {
+        if(str[i]!=str[l-1-i])
+            {flag=1;
+            break;
+            }
+    }
+    if(flag==0)
+        cout<<"the string is palindrome"<<endl;
+    else
+        cout<<"the string is not palindrome"<<endl;
+        return 0;
+}
